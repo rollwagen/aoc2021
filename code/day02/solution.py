ece17d9bc680aab99a6c6c84e646e14a3019ec70
rollwagen <rollwagen@gmail.com>
2021-12-01 20:06:22 +0100

    Initial commit of skeleton

---
+++ code/day02/solution.py
@@ -0,0 +1,14 @@
+input = [int(line.strip()) for line in open("input").readlines()]
+
+
+def part_one():
+    pass
+
+
+def part_two():
+    pass
+
+
+if __name__ == "__main__":
+    print(f"{part_one()=}")
+    print(f"{part_two()=}")
