219b6c8ade7edb0b8e55b7f2216450d9ff8bda64
rollwagen <rollwagen@gmail.com>
2021-12-02 07:31:41 +0100

    Add solutions for day two

--- code/day02/solution.py
+++ code/day02/solution.py
@@ -1,14 +1,51 @@
-input = [int(line.strip()) for line in open("input").readlines()]
+from typing import NamedTuple
+
+
+class Command(NamedTuple):
+    direction: str  # forward, down, up
+    unit: int
+
+    @classmethod
+    def from_string(cls, string):
+        direction, unit = string.split(" ")
+        return cls(direction, int(unit))
+
+
+input = [Command.from_string(line) for line in open("input").readlines()]
 
 
 def part_one():
-    pass
+    horizontal_position = 0
+    depth = 0
+
+    for command in input:
+        if command.direction == "forward":
+            horizontal_position += command.unit
+        elif command.direction == "down":
+            depth += command.unit
+        elif command.direction == "up":
+            depth -= command.unit
+
+    return horizontal_position * depth
 
 
 def part_two():
-    pass
+    horizontal_position = 0
+    depth = 0
+    aim = 0
+
+    for command in input:
+        if command.direction == "forward":
+            horizontal_position += command.unit
+            depth += command.unit * aim  # increase depth by aim * X
+        elif command.direction == "down":
+            aim += command.unit  # increases your aim by units.
+        elif command.direction == "up":
+            aim -= command.unit  # decreases your aim by units
+
+    return horizontal_position * depth
 
 
 if __name__ == "__main__":
-    print(f"{part_one()=}")
-    print(f"{part_two()=}")
+    print(f"{part_one()=} (2036120)")
+    print(f"{part_two()=} (2015547716)")
