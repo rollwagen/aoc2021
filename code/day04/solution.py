9380aca8ff81cb1b9cebe9bfb1c7323d0c8e2cec
rollwagen <rollwagen@gmail.com>
2021-12-04 08:35:04 +0100

    Add skeleton for day four

---
+++ code/day04/solution.py
@@ -0,0 +1,16 @@
+
+
+input = [line.strip() for line in open("input").readlines()]
+
+
+def part_one() -> int:
+    pass
+
+
+def part_two() -> int:
+    pass
+
+
+if __name__ == "__main__":
+    print(f"{part_one()=}")
+    print(f"{part_two()=}")
