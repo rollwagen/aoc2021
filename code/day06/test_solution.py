08b6a745dae0d3a2d44ad3534d2ebfd52bf93d41
rollwagen <rollwagen@gmail.com>
2021-12-06 11:58:05 +0100

    Solutions for Day 6

--- code/day06/test_solution.py
+++ code/day06/test_solution.py
@@ -5,7 +5,7 @@ from solution import part_one, part_two
 
 class Day06Test(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(-1, part_one())
+        self.assertEqual(379414, part_one())
 
     def test_part_two(self):
-        self.assertEqual(-1, part_two())
+        self.assertEqual(1705008653296, part_two())
