9088bcdad6c25d0eafb04bcdf5981b76f1f647f0
rollwagen <rollwagen@gmail.com>
2021-12-06 08:13:57 +0100

    Day 06 skeleton

---
+++ code/day06/solution.py
@@ -0,0 +1,16 @@
+input = [line.strip() for line in open("input.sample").readlines()]
+
+
+def part_one() -> int:
+    """ """
+    pass
+
+
+def part_two() -> int:
+    """ """
+    pass
+
+
+if __name__ == "__main__":
+    print(f"{part_one()=}  (sample: )")
+    print(f"{part_two()=}  (sample: ")
