87db14ef3171bdb1d5352ef352790cfa0f5ecd05
rollwagen <rollwagen@gmail.com>
2021-12-07 09:50:04 +0100

    Initial skeleton for day 7

---
+++ src/day07/solution.py
@@ -0,0 +1,18 @@
+crab_positions = [int(pos) for pos in open("input.sample").read().split(",")]
+
+
+def part_one() -> int:
+    """ """
+    print(f"{crab_positions=}")
+    pass
+    return 0
+
+
+def part_two() -> int:
+    pass
+    return 0
+
+
+if __name__ == "__main__":
+    print(f"{part_one()=}  (sample: )")
+    print(f"{part_two()=}  (sample: )")
