bcd2533d0c1b530531a2eb85e6bd2a624374d43f
rollwagen <rollwagen@gmail.com>
2021-12-08 08:19:35 +0100

    Skeleton for day 8

---
+++ src/day08/solution.py
@@ -0,0 +1,16 @@
+line = [line for line in open("input.sample").read().split(",")]
+
+
+def part_one() -> int:
+    """ """
+    return 0
+
+
+def part_two() -> int:
+    """ """
+    return 0
+
+
+if __name__ == "__main__":
+    print(f"{part_one()=}  (sample: )")
+    print(f"{part_two()=}  (sample: )")
