e4b27c71ad00a3d283bae224c777fd5cd9443e03
rollwagen <rollwagen@gmail.com>
2021-12-08 12:51:57 +0100

    Soltion for part one for day 8

--- src/day08/solution.py
+++ src/day08/solution.py
@@ -1,9 +1,23 @@
-line = [line for line in open("input.sample").read().split(",")]
+input = [line.strip() for line in open("input").readlines()]
+
+
+segments_to_digit = {
+    2: 1,  # used two segments (c and f)
+    4: 4,  # uses four segments
+    3: 7,  # uses three segments
+    7: 8,  # uses all seven segments
+}
 
 
 def part_one() -> int:
-    """ """
-    return 0
+    appearances = 0
+    for entry in input:
+        signal_patterns, output_values = (part.strip() for part in entry.split("|"))
+        for value in output_values.split():
+            if segments_to_digit.get(len(value)) is not None:
+                appearances += 1
+
+    return appearances
 
 
 def part_two() -> int:
@@ -12,5 +26,5 @@ def part_two() -> int:
 
 
 if __name__ == "__main__":
-    print(f"{part_one()=}  (sample: )")
+    print(f"{part_one()=} 543 (sample: 26)")
     print(f"{part_two()=}  (sample: )")
--- src/day08/test_solution.py
+++ src/day08/test_solution.py
@@ -5,7 +5,7 @@ from solution import part_one, part_two
 
 class Day08Test(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(-1, part_one())
+        self.assertEqual(543, part_one())
 
     def test_part_two(self):
         self.assertEqual(-1, part_two())
