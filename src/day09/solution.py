d2c08c8b3c2b3729d5c1908a126c832b69bfe4f9
rollwagen <rollwagen@gmail.com>
2021-12-09 14:21:02 +0100

    Day 9 solution for part two

--- src/day09/solution.py
+++ src/day09/solution.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 input = [line.strip() for line in open("input").readlines()]
 
 """
@@ -14,32 +16,62 @@ for y, line in enumerate(input):
         grid[(x, y)] = int(value)
 
 
-def part_one() -> int:
-    """
-    Find the low points - the locations that are lower than any of its adjacent
-    locations. Most locations have four adjacent locations (up, left, right, and
-    down); locations on the edge or corner of the map have three or two adjacent
-    locations, respectively. The risk level of a low point is 1 plus its height.
-    """
+def part_one_and_two():
+
     neighbors = [(0, -1), (-1, 0), (1, 0), (0, 1)]  # above, left, right, below
 
     def _get_neighbour_values(x: int, y: int) -> list:
         return [grid.get((x + dx, y + dy), float("inf")) for dx, dy in neighbors]
 
+    low_points = []
     risk_level_sum = 0
     for point, height in grid.items():
         if grid[point] < min(_get_neighbour_values(*point)):
+            low_points.append(point)
             risk_level_sum += height + 1
 
-    return risk_level_sum
+    print(f"Part One: {risk_level_sum=}")  # ---------------------------------
 
+    def _get_neighbour_points(x: int, y: int) -> list:
+        return [(x + dx, y + dy) for dx, dy in neighbors]
 
-def part_two() -> int:
-    """ """
-    return 0
+    basins = []
+
+    for low_point in low_points:
+
+        queue = deque()
+        queue.append(low_point)
+
+        visited = []
+        visited.append(low_point)
+
+        basin_values = []
+        basin_values.append(grid.get(low_point))  # basin size includes low point
+
+        while queue:
+            point = queue.pop()
+            for adjacent_point in _get_neighbour_points(*point):
+                value_adjacent_point = grid.get(adjacent_point, None)
+                print(f"{point=} {value_adjacent_point=}")
+                if (
+                    value_adjacent_point is None
+                    or value_adjacent_point == 9
+                    or adjacent_point in visited
+                ):
+                    pass
+                else:
+                    basin_values.append(value_adjacent_point)
+                    visited.append(adjacent_point)
+                    queue.append(adjacent_point)
+
+        basins.append(basin_values)
+
+    # three largest basins and multiply their sizes together
+    basins.sort(key=len, reverse=True)
+    basin_sum = len(basins[0]) * len(basins[1]) * len(basins[2])
+    print(f"Part Two: {basin_sum}  858494 (sample: 1134)")
 
 
 if __name__ == "__main__":
     print("--- Day 9: Smoke Basin ---")
-    print(f"{part_one()=}  594 (sample: [1, 0, 5, 5], 15)")
-    print(f"{part_two()=}  (sample: )")
+    part_one_and_two()
