aac4443722c6dfd495c74f0f17806c774a900e21
rollwagen <rollwagen@gmail.com>
2021-12-13 08:11:31 +0100

    Add template

---
+++ src/template/solution.py
@@ -0,0 +1,21 @@
+import pathlib
+
+
+def parse(input: str):
+    return None
+
+
+def part_one(data) -> int:
+    return 0
+
+
+def part_two(data) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    print("--- Day __: _______ _______ ---")
+    input = pathlib.Path("input").read_text().strip()
+    data = parse(input)
+    print(f"{part_one(data)=}  answer: (sample: )")
+    print(f"{part_two(data)=}  answer  (sample: )")
