a0f898e8d53860f14ebe983dbfdf194f5cc0be85
rollwagen <rollwagen@gmail.com>
2021-12-13 08:39:31 +0100

    Add skeleton/template for day 13

---
+++ src/day13/solution.py
@@ -0,0 +1,25 @@
+import pathlib
+
+
+def parse(input: str):
+    return None
+
+
+def part_one(data) -> int:
+    """
+    How many dots are visible after completing **just the first**
+    fold instruction on your transparent paper?
+    """
+    return 0
+
+
+def part_two(data) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    print("--- Day __: _______ _______ ---")
+    input = pathlib.Path("input").read_text().strip()
+    data = parse(input)
+    print(f"{part_one(data)=}  answer: (sample: 17)")
+    print(f"{part_two(data)=}  answer  (sample: )")
