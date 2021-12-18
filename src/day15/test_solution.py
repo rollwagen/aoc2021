31eef8ddc1ca33a669fd68f2d4266f4029f17f57
rollwagen <rollwagen@gmail.com>
2021-12-18 20:08:20 +0100

    Day 15 part one and two

--- src/day15/test_solution.py
+++ src/day15/test_solution.py
@@ -8,28 +8,27 @@ DIR = pathlib.Path(__file__).parent
 
 @pytest.fixture
 def sample_input():
-    input = (DIR / "input.sample").read_text().strip()
-    return s.parse(input)
+    input_ = [line.strip() for line in open((DIR / "input.sample")).readlines()]
+    return s.parse(input_)
 
 
 @pytest.fixture
 def puzzle_input():
-    input = (DIR / "input").read_text().strip()
-    return s.parse(input)
+    input_ = [line.strip() for line in open((DIR / "input")).readlines()]
+    return s.parse(input_)
 
 
 def test_parse_sample(sample_input):
     """Test that input is parsed properly"""
-    assert 0 == 1
+    assert len(sample_input) == 100
 
 
 def test_part_one_sample(sample_input):
     assert s.part_one(sample_input) == 40
 
 
-@pytest.mark.skip
 def test_part_one_puzzle(puzzle_input):
-    assert s.part_one(puzzle_input) == -1
+    assert s.part_one(puzzle_input) == 748
 
 
 @pytest.mark.skip
